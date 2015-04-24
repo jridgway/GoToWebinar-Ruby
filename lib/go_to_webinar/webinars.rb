d089ee76631e1be587e36ee20ba8541ff599c02c
Joseph Ridgway <joe.e.ridgway@gmail.com>
2015-04-24 13:22:38 -0400

    Added create webinar method

--- lib/go_to_webinar/webinars.rb
+++ lib/go_to_webinar/webinars.rb
@@ -1,25 +1,29 @@
 module GoToWebinar
   module Webinars
-    
+
+    def create_webinar(params = {})
+      self.class.post("webinars", :query => params)
+    end
+
     def get_historical_webinars(params = {})
       self.class.get("historicalWebinars", :query => params)
     end
-    
+
     def get_upcoming_webinars(params = {})
       self.class.get("upcomingWebinars", :query => params)
     end
-    
+
     def get_webinar(webinar_key)
       self.class.get("webinars/#{webinar_key}")
     end
-    
+
     def get_webinar_meeting_times(webinar_key)
       self.class.get("webinars/#{webinar_key}/meetingTimes")
     end
-    
+
     def get_webinars()
       self.class.get("webinars")
     end
-    
+
   end
 end
